b3f5855931727bdccba1c161b3aa8268a5ced3a7
Pierric Buchez <contact@myecoria.com>
2025-06-29 23:07:41 +0200

    [~] release.sh: Update release script to change password prompt language and remove key generation commands

--- release.sh
+++ release.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-read -s -p "Entrez le mot de passe pour la clé : " KEY_PASS
+read -s -p "Password for the key: " KEY_PASS
 echo
-
-keytool -genkeypair -v \
-    -keystore epico_key.jks \
-    -keyalg RSA \
-    -keysize 2048 \
-    -validity 10000 \
-    -alias epico \
-    -storepass "$KEY_PASS" \
-    -keypass "$KEY_PASS" \
-    -dname "CN=epico, OU=epico, O=epico, L=Paris, S=IDF, C=FR"
 
 mv epico_key.jks android/app/epico_key.jks
 export KEY_PASSWORD="$KEY_PASS"
